a3d6144c12e08c0d08402552ddb41ec24a5c182e
Massimiliano Bernabé <massimiliano.bernabe@gmail.com>
2016-12-09 14:07:40 +0100

    Delete 104_idtiles_generation.bat

--- scripts/104_idtiles_generation.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo 0-5 for the whole world
-echo select manager.bbox2tiles(0,5,-85.0511,85.0511,-180.0,180.0,'C:/osm4dys/Website/idmap'); | "C:\osm4dys\PostgreSQL\9.3\bin\psql.exe" -d osm4dys -U postgres
-
-echo 6-15 for berlin
-echo select manager.bbox2tiles(6,15,52.32,52.71,13.04,13.81,'C:/osm4dys/Website/idmap'); | "C:\osm4dys\PostgreSQL\9.3\bin\psql.exe" -d osm4dys -U postgres
-echo TODO
-pause
